a782fe76b53d49979a9bb150ebe6ef6cd90c0aa3
BKGSr1 <164350063+BKGSr1@users.noreply.github.com>
2024-04-22 12:16:00 +0000

    :)

--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
+function toggleButton(){
+    if(document.getElementById("txtSearch").value == "")
+    {
+        document.getElementById("btnSearch").disabled = true;
+    }
+    else{
+        document.getElementById("btnSearch").disabled = false;
+    }
+}
 
+function getDate(){
+    // TBD display current date in the span tag with id="date" in web page footer
+    // hint: https://www.w3schools.com/js/tryit.asp?filename=tryjs_date_current
+    document.getElementById("date").innerHTML = new Date();
+}
